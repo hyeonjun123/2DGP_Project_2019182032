b3ca514256f247b80f70205ec7c72ab9f23b587e
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-10-13 16:19:32 +0900

    화면에 볼을 그렸다.

--- starter.py
+++ starter.py
@@ -10,17 +10,19 @@ dir_y, dir_y2 = 0, 0
 
 x,x2 = 170, 680
 y,y2 = 197.5, 197.5
+bx,by = 412, 400
 
 
 def load_resources():
     global volleyball_ground, volleyball_net, arrow, king_icon
     global character, character2
+    global ball
 
     arrow = load_image('hand_arrow.png')
     volleyball_ground = load_image('Evening_background3.png')
     volleyball_net = load_image('volleyball_net_1.png')
 
-
+    ball = load_image('ball.png')
     character = load_image('animation_sheet4.png') #사람
     character2 = load_image('animation_sheet4.png') #마법사#
 
@@ -132,8 +134,9 @@ def render_world():
     clear_canvas()
     volleyball_ground.draw(TUK_WIDTH //2, TUK_HEIGHT//2)
     volleyball_net.draw(412, 225)
-
+    ball.draw(bx,by)
     #king_icon.draw(512,512)
+
 
 #character1 render
     if idle_1 == True:
@@ -142,13 +145,13 @@ def render_world():
     elif idle_1==False:
         character.clip_draw(frame * 120, 0, 120, 130, x, y)
 
-
-
 #character2 render
     if idle_2 == True:
         character2.clip_draw(0, 260, 120, 130, x2, y2)
     elif idle_2==False:
             character2.clip_draw(frame * 120, 260, 120, 130, x2, y2)
+
+
     update_canvas()
 
 
@@ -157,11 +160,13 @@ def render_world():
 def update_world():
     global frame
     global t, action
-    global x, y, x2, y2 #화면상에 그려질 위치
+    global x, y, x2, y2,bx,by#화면상에 그려질 위치
     frame = (frame + 1) % 8
 
     x += dir_x * 5
     y += dir_y * 5
+    by -= 1 #볼 떨어지도록
+
 
     #화면 밖으로 나가지 못하게
     if (80 <= x2 <= 1200 and 100 <= y2 <= 970):
@@ -178,6 +183,7 @@ def update_world():
             y2 = 100 + 10 #밑
         elif(y2 > 970):
             y2 = 970 - 10 #위
+
 
     delay(0.01)
 

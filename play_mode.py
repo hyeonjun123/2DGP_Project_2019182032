b1e37283047db37e26f2eefe66b0ad404dad55a7
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-11-05 23:42:56 +0900

    게임월드코드 추가

---
+++ play_mode.py
@@ -0,0 +1,50 @@
+from pico2d import *
+
+import game_world
+from boy import Boy
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
+
+
+def init():
+    global running
+    global grass
+    global team
+    global boy
+
+    running = True
+
+    game_world.add_object(grass, 0)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+
+def update():
+    game_world.update()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    pass
+
+
